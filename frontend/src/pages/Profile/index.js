3d5644d4e25087830612f2e2a32027c3be75a5d4
Renan Evangelista Pereira <renanevangelistapereira@gmail.com>
2020-03-26 08:21:15 -0300

    Iniciando Profile

---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { FiArrowLeft } from 'react-icons/fi';
+
+import { Link } from 'react-router-dom';
+
+import './styles.css';
+
+import logoImg from '../../assets/logo.svg';
+import heroesImg from '../../assets/heroes.png';
+
+export default function Profile(){
+    return (
+        <div className="profile-container">
+
+        </div>
+    );
+}
