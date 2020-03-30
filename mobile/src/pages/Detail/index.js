65772e713cc9dc6ad483076c7f7770af594939f0
Renan Evangelista Pereira <renanevangelistapereira@gmail.com>
2020-03-29 21:43:31 -0300

    Mobile Pagina Detalhes

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
 import logoImg from '../../assets/logo.png';
 
 import styles from './styles';
 
 export default function Detail() {
+
+    const navigation = useNavigation();
+
+    function navigateToIncidents(){
+        navigation.navigate('Incidents');
+    }
+
     return (
         <View style={styles.container}>
+            <View style={styles.header}>
                 <Image source={logoImg} />
-                <Text>
-                    Detalhe
-                </Text>
+                
+                <TouchableOpacity onPress={navigateToIncidents}>
+                    <Feather name="arrow-left" size={28} color="#e82041" />
+                </TouchableOpacity>
+            </View>
+            <View style={styles.incident}>
+                    <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
+                    <Text style={styles.incidentValue}>APAD</Text>
+                    
+                    <Text style={styles.incidentProperty}>Caso:</Text>
+                    <Text style={styles.incidentValue}>Xornder lander uonder</Text>
+                    
+                    <Text style={styles.incidentProperty}>Valor:</Text>
+                    <Text style={styles.incidentValue}>R$ 120,00</Text>
+             </View>
+             <View style={styles.contactBox}>
+                <Text style={styles.heroTitle}>Salve o Dia</Text>
+                <Text style={styles.heroTitle}>Seja o heroi deste caso</Text>
+
+                <Text style={styles.heroTitle}>Entre em contato:</Text>
+
+                <View style={styles.actions}>
+                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                        <Text style={styles.actionText}>Whatsapp</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.action} onPress={() => {}}>
+                        <Text style={styles.actionText}>Email</Text>
+                    </TouchableOpacity>
+                </View>
+             </View>
         </View>
     );
 }
--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -32,7 +32,7 @@ export default function Incidents() {
                 keyExtractor={incident => String(incident)}
                 showsVerticalScrollIndicator={false}
                 renderItem={() => (
-                    <View style={styles.incident}>
+                <View style={styles.incident}>
                     <Text style={styles.incidentProperty}>ONG:</Text>
                     <Text style={styles.incidentValue}>APAD</Text>
                     
@@ -42,7 +42,7 @@ export default function Incidents() {
                     <Text style={styles.incidentProperty}>Valor:</Text>
                     <Text style={styles.incidentValue}>R$ 120,00</Text>
 
-                    <TouchableOpacity style={styles.detailsButton} onPress={ () => {} }>
+                    <TouchableOpacity style={styles.detailsButton} onPress={navigateToDetail}>
                         <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
                         <Feather name="arrow-right" size={16} color="#e02041"></Feather>
                     </TouchableOpacity>
