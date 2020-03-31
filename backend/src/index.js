9908a3f3ad431314030a97b3339eceb4f44945ce
Renan Evangelista Pereira <renanevangelistapereira@gmail.com>
2020-03-30 21:13:46 -0300

    Validacoes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const rotas = require('./routes');
 
 const app = express();
@@ -12,6 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(rotas);
+app.use(errors());
 
 app.listen(33033);
 
